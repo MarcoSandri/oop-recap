dc2340b8529854a30c49e75bc5f78dc6bb78401f
Marco Sandri <sandrimarco98@gmail.com>
2022-03-17 18:31:01 +0100

    declaring parent

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-    
+    //richiamo il file contenente le classi
+    require_once __DIR__ . '/classes/parents.php';
+    require_once __DIR__ . '/classes/child.php';
+
+    //creo un nuovo oggetto
+    $parent = new parents('public', );
+    var_dump($parent);
+
 
 ?>
