2cdff7232c996aaa1d239d361072c511c4126a9e
Marco Sandri <sandrimarco98@gmail.com>
2022-03-17 17:52:12 +0100

    parent

--- classes/parent.php
+++ classes/parent.php
@@ -7,21 +7,24 @@
         protected $protected; //proprietà accessibile solo dall'interno della classe, ereditata
         private $private; //proprietà accessibile solo dalla classe dove è stata dichiarata, non è ereditata
 
-        //funzione costruttore: viene invocata quando l'oggetto viene creato, permette di eseguire azioni alla creazione
-        public function __construct($public){
+        //metodo costruttore: viene invocata quando l'oggetto viene creato, permette di eseguire azioni alla creazione.
+        //è possibile assegnare un valore ad una proprietà all'interno degli argomenti del metodo, rendendo opzionale la sua compilazione quando viene chiamato il metodo
+        public function __construct($public, $private = "placeholder"){
 
             //Assegnazione di un valore ad una proprietà
             $this->public = $public;
+
+            $this->private = $private;
             
         }
 
-        //funzione set: generalmente utilizzata per assegnare un valore ad una proprietà
+        //metodo set: generalmente utilizzata per assegnare un valore ad una proprietà
         public function setProtected($protected) {
 
             $this->protected = $protected;
         }
 
-        //funzione get: generalmente utilizzata per ricevere il valore di una proprietà
+        //metodo get: generalmente utilizzata per ricevere il valore di una proprietà
         public function getProtected($protected) {
 
             return $this->protected;
