78b184e44e4c11fabcd8d2a97d272a6e445d9353
Marco Sandri <sandrimarco98@gmail.com>
2022-03-17 18:30:54 +0100

    traits

---
+++ traits/traits.php
@@ -0,0 +1,14 @@
+<?php
+
+    //i trait sono un'insieme di proprietà e metodi applicabili a più classi
+    trait traits {
+
+        public $traitProperty;
+
+        public function setTraitProperty($traitProperty) {
+
+            $this->traitProperty = $traitProperty;
+        }
+    }
+
+?>
